d170092695796e4e5445941376364a80ed3682f8
joshitha0818 <joshitha0818@gmail.com>
2025-12-12 11:54:23 +0530

    feat: add bottom navigation bar and cart page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/global_vairable.dart';
 import 'package:shopping_app/product_cart.dart';
+import 'package:shopping_app/product_details_page.dart';
+import 'package:shopping_app/cart_page.dart'; // <-- Added
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -26,87 +28,120 @@ class _HomePageState extends State<HomePage> {
       borderRadius: BorderRadius.horizontal(left: Radius.circular(50)),
     );
 
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: [
-            Row(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    'Shoes \nCollection',
-                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  'Shoes \nCollection',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+              const Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'search',
+                    prefixIcon: Icon(Icons.search),
+                    border: border,
+                    enabledBorder: border,
+                    focusedBorder: border,
                   ),
                 ),
-                Expanded(
-                  child: TextField(
-                    decoration: const InputDecoration(
-                      hintText: 'search',
-                      prefixIcon: Icon(Icons.search),
-                      border: border,
-                      enabledBorder: border,
-                      focusedBorder: border,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 120,
-              child: ListView.builder(
-                itemCount: filters.length,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  final filter = filters[index];
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedFilter = filter;
-                      });
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Chip(
-                        backgroundColor: selectedFilter == filter
-                            ? Theme.of(context).colorScheme.onPrimary
-                            : const Color.fromRGBO(245, 247, 249, 1),
-                        side: const BorderSide(
-                          color: Color.fromRGBO(245, 247, 249, 1),
-                        ),
-                        label: Text(filter),
-                        labelStyle: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 20,
-                          vertical: 15,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 120,
+            child: ListView.builder(
+              itemCount: filters.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                final filter = filters[index];
+
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedFilter = filter;
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Chip(
+                      backgroundColor: selectedFilter == filter
+                          ? Theme.of(context).colorScheme.onPrimary
+                          : const Color.fromARGB(255, 246, 150, 150),
+                      side: const BorderSide(
+                        color: Color.fromRGBO(245, 247, 249, 1),
+                      ),
+                      label: Text(filter),
+                      labelStyle: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 15,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
                       ),
                     ),
-                  );
-                },
-              ),
+                  ),
+                );
+              },
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: product.length,
-                itemBuilder: (context, index) {
-                  final products = product[index];
-                  return ProductCart(
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: product.length,
+              itemBuilder: (context, index) {
+                final products = product[index];
+
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            ProductDetailsPage(product: products),
+                      ),
+                    );
+                  },
+                  child: ProductCart(
                     title: products['title'] as String,
                     price: products['price'] as double,
                     image: products['imageurl'] as String,
-                  );
-                },
-              ),
+                    backgroundColor: index.isEven
+                        ? const Color.fromARGB(255, 66, 119, 147)
+                        : const Color.fromARGB(255, 175, 116, 116),
+                  ),
+                );
+              },
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: "Cart",
+          ),
+        ],
+        onTap: (index) {
+          if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const CartPage()),
+            );
+          }
+        },
       ),
     );
   }
