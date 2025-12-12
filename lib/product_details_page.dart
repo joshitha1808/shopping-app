478a5e8c9e6b4140ce1a09ff13bd75fcc1ee5814
joshitha0818 <joshitha0818@gmail.com>
2025-12-12 11:42:06 +0530

    feat: add size selection and add-to-cart

---
+++ lib/product_details_page.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailsPage extends StatefulWidget {
+  final Map<String, Object> product;
+  const ProductDetailsPage({super.key, required this.product});
+
+  @override
+  State<ProductDetailsPage> createState() => _ProductDetailsPageState();
+}
+
+class _ProductDetailsPageState extends State<ProductDetailsPage> {
+  int selectedSize = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text('Details'))),
+
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              widget.product['title'] as String,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Image.asset(widget.product['imageurl'] as String),
+            ),
+
+            SizedBox(height: 30),
+
+            Container(
+              padding: EdgeInsets.all(20.0),
+              height: 300,
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(211, 220, 230, 1),
+                borderRadius: BorderRadius.circular(40),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Text(
+                      '\$ ${widget.product['price']}',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ),
+
+                  SizedBox(height: 10),
+
+                  SizedBox(
+                    height: 80,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount:
+                            (widget.product['sizes'] as List<int>).length,
+                        itemBuilder: (context, index) {
+                          final size =
+                              (widget.product['sizes'] as List<int>)[index];
+
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  selectedSize = size;
+                                });
+                              },
+                              child: Chip(
+                                label: Text(size.toString()),
+                                backgroundColor: selectedSize == size
+                                    ? Theme.of(context).colorScheme.primary
+                                    : null,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                        minimumSize: const Size(double.infinity, 50),
+                      ),
+                      child: Text(
+                        'Add to cart',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
